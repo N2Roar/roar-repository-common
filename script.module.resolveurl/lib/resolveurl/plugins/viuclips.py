b77c5c733849fd9009ecb477e841f666c722b373
SomOne <NVUGiLjnMCrPU@gmail.com>
2019-08-22 03:42:16 -0700

    - update res

--- script.module.resolveurl/lib/resolveurl/plugins/viuclips.py
+++ script.module.resolveurl/lib/resolveurl/plugins/viuclips.py
@@ -1,6 +1,6 @@
-'''
-    urlresolver Kodi plugin
-    Copyright (C) 2019
+"""
+    plugin in for ResolveUrl
+    Copyright (C) 2019 gujal
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,37 +14,31 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+"""
 
 import re
 from lib import helpers
-from urlresolver import common
-from urlresolver.resolver import UrlResolver, ResolverError
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
 
-
-class VeryStreamResolver(UrlResolver):
-    name = "verystream"
-    domains = ["verystream.com", "verystream.xyz"]
-    pattern = r'(?://|\.)(verystream\.(?:com|xyz))/(?:stream|e|source)/([a-zA-Z0-9]+)'
+class ViuclipsResolver(ResolveUrl):
+    name = "viuclips"
+    domains = ["viuclips.net", "veuclips.com"]
+    pattern = r'(?://|\.)(v[ie]uclips\.(?:net|com))/(?:embed/)?([0-9a-zA-Z]+)'
 
     def __init__(self):
         self.net = common.Net()
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.FF_USER_AGENT, 'Referer': 'https://verystream.com'}
+        headers = {'User-Agent': common.FF_USER_AGENT}
         response = self.net.http_GET(web_url, headers=headers)
         html = response.content
+        if 'video has been removed' not in html:
+            source = re.findall('hls:"([^"]+)', html)[0]
+            return source + helpers.append_headers({'User-Agent': common.FF_USER_AGENT})
 
-        if html:
-            regex = '(%s~[~.:a-zA-Z0-9]+)' % media_id
-            videolink = re.search(regex, html)
-            if videolink:
-                source = 'https://verystream.com/gettoken/%s?mime=true' % videolink.group(1)
-                headers.update({'Referer': web_url})
-                return source + helpers.append_headers(headers)
-
-        raise ResolverError("Could not locate video")
+        raise ResolverError('File Not Found or removed')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, 'https://verystream.com/e/{media_id}')
+        return self._default_get_url(host, media_id, template='https://player.viuclips.net/embed/{media_id}')
