aafd586ba6625d1be3e0a8e8e522fa46a328b626
SomOne <NVUGiLjnMCrPU@gmail.com>
2019-11-10 18:49:26 -0800

    - update res

--- script.module.resolveurl/lib/resolveurl/plugins/bitchute.py
+++ script.module.resolveurl/lib/resolveurl/plugins/bitchute.py
@@ -1,6 +1,7 @@
 """
-    Plugin for UrlResolver
-    Copyright (C) 2019 gujal
+    ResolveURL Kodi module
+    Bitchute plugin
+    Copyright (C) 2019 twilight0
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,30 +16,36 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-import re
-from lib import helpers
-from urlresolver import common
-from urlresolver.resolver import UrlResolver, ResolverError
 
-class ClipWatchingResolver(UrlResolver):
-    name = "clipwatching"
-    domains = ['clipwatching.com']
-    pattern = r'(?://|\.)(clipwatching\.com)/(?:embed-)?(\w+)'
+from __resolve_generic__ import ResolveUrl
+from lib import helpers
+from resolveurl import common
+
+
+class BitchuteResolver(ResolveUrl):
+
+    name = "bitchute.com"
+    domains = ['bitchute.com']
+    pattern = r'(?://|\.)(bitchute\.com)/(?:video|embed)/([\w-]+)/'
 
     def __init__(self):
+
         self.net = common.Net()
-        
+        self.headers = {'User-Agent': common.RAND_UA}
+
     def get_media_url(self, host, media_id):
+
         web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.RAND_UA}
-        html = self.net.http_GET(web_url, headers=headers).content
-        
-        html += helpers.get_packed_data(html)
-        source = re.search(r'''sources\s*:\s*\["([^"]+)''', html)
-        if source:
-            return source.group(1) + helpers.append_headers(headers)
-                
-        raise ResolverError("Video not found")
-        
+        response = self.net.http_GET(web_url, headers=self.headers)
+
+        sources = helpers.scrape_sources(
+            response.content, patterns=[r'''source src=['"](?P<url>https.+?\.mp4)['"] type=['"]video/mp4['"]''']
+        )
+
+        self.headers.update({'Referer': web_url})
+
+        return helpers.pick_source(sources) + helpers.append_headers(self.headers)
+
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
+
+        return self._default_get_url(host, media_id, 'https://www.{host}/video/{media_id}')
--- script.module.resolveurl/lib/resolveurl/plugins/streamty.py
+++ script.module.resolveurl/lib/resolveurl/plugins/streamty.py
@@ -1,6 +1,7 @@
-"""
-    Plugin for UrlResolver
-    Copyright (C) 2019 gujal
+# -*- coding: utf-8 -*-
+'''
+    resolveurl Kodi plugin
+    Copyright (C) 2018
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,31 +15,34 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+'''
+ 
 import re
 from lib import helpers
-from urlresolver import common
-from urlresolver.resolver import UrlResolver, ResolverError
+from lib import jsunpack
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
 
-class ClipWatchingResolver(UrlResolver):
-    name = "clipwatching"
-    domains = ['clipwatching.com']
-    pattern = r'(?://|\.)(clipwatching\.com)/(?:embed-)?(\w+)'
+class StreamtyResolver(ResolveUrl):
+    name = "streamty"
+    domains = ["streamty.com"]
+    pattern = '(?://|\.)(streamty\.com)/(?:embed-)?([0-9a-zA-Z]+)'
 
     def __init__(self):
         self.net = common.Net()
-        
+
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.RAND_UA}
+        headers = {'User-Agent': common.RAND_UA, 'Referer': web_url}
         html = self.net.http_GET(web_url, headers=headers).content
-        
-        html += helpers.get_packed_data(html)
-        source = re.search(r'''sources\s*:\s*\["([^"]+)''', html)
-        if source:
-            return source.group(1) + helpers.append_headers(headers)
-                
-        raise ResolverError("Video not found")
-        
+        r = re.search("text/javascript'>(eval.*?)\s*</script>", html, re.DOTALL)
+        if r:
+            html = jsunpack.unpack(r.group(1))
+            src = re.search('file:"([^"]+)"',html)
+            if src:
+                return src.group(1) + helpers.append_headers(headers)
+        raise ResolverError('Video cannot be located.')
+ 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
+
