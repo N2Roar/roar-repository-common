7649e297fc269169087fc81bf758f4ac38244a74
SomOne <NVUGiLjnMCrPU@gmail.com>
2019-12-20 03:20:13 -0800

    - update res

--- script.module.resolveurl/lib/resolveurl/plugins/streamz.py
+++ script.module.resolveurl/lib/resolveurl/plugins/streamz.py
@@ -1,42 +1,43 @@
 """
-    Kodi urlresolver plugin
-    Copyright (C) 2019
+    Plugin for ResolveUrl
+    Copyright (C) 2019 gujal
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
+
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-import re
-from urlresolver import common
-from urlresolver.resolver import UrlResolver, ResolverError
-from lib import jsunpack
+from lib import helpers
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
 
-class MixdropResolver(UrlResolver):
-    name = "mixdrop"
-    domains = ["mixdrop.co"]
-    pattern = r'(?://|\.)(mixdrop\.co)/(?:f|e)/(\w+)'
+
+class StreamzResolver(ResolveUrl):
+    name = "streamz"
+    domains = ['streamz.cc']
+    pattern = r'(?://|\.)(streamz\.cc)/([0-9a-zA-Z]+)'
 
     def __init__(self):
         self.net = common.Net()
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
-        headers = {'Origin':'https://mixdrop.co','Referer':'https://mixdrop.co/e/' + media_id,'User-Agent': common.RAND_UA}
+        headers = {'User-Agent': common.RAND_UA}
         html = self.net.http_GET(web_url, headers=headers).content
+        if 'File not found, sorry!' not in html:
+            sources = helpers.scrape_sources(html)
+            if sources:
+                return helpers.pick_source(sources) + helpers.append_headers(headers)
 
-        try:
-            r = re.search('\s+?(eval\(function\(p,a,c,k,e,d\).+)\s+?', html)
-            r = jsunpack.unpack(r.group(1))
-            r = re.search('vsrc?="([^"]+)', r.replace('\\', ''))
-            return "https:" + r.group(1)
-        except:
-            raise ResolverError("Video not found")
+        raise ResolverError("Video not found")
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/e/{media_id}')
+        return self._default_get_url(host, media_id, template='https://{host}/{media_id}')
